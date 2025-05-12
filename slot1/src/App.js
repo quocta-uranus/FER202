13626fe3d5c74575fbffb84fbc7fe9af2a1d0e5c
Leo AQ <leoaq@Leos-MacBook-Pro.local>
2025-05-12 16:54:06 +0700

    push code slot 1

---
+++ slot1/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div> Create react app</div>
+      <p>Hello word 123567</p>
+    </div>
+  );
+}
+
+export default App;
